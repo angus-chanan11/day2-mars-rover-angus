ccf654b96f7b38961701daa498604f276f65b1dd
angus-chanan11 <angus.chan@oocl.com>
2024-11-19 17:46:19 +0800

    [MarsRover] feat: move forward with direction N

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -3,13 +3,15 @@ package org.example;
 public class MarsRover {
 
     private Orientation direction;
+    private int yLocation;
 
     public MarsRover(){
         direction = new NorthOrientation();
+        yLocation = 0;
     }
 
     public String showStatus() {
-        return "0:0:" + direction.getDirection();
+        return "0:" + yLocation + ":" + direction.getDirection();
     }
 
     public String executeCommand(String command) {
@@ -17,8 +19,10 @@ public class MarsRover {
             direction = direction.turnLeft();
         } else if (command.equals("R")) {
             direction = direction.turnRight();
+        } else if (command.equals("M")) {
+            yLocation += 1;
         }
 
-        return "0:0:" + direction.getDirection();
+        return "0:" + yLocation + ":" + direction.getDirection();
     }
 }
