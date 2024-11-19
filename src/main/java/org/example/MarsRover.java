42f7b71890b1f7891c1ea11d6e88ac1fe491cd5d
angus-chanan11 <angus.chan@oocl.com>
2024-11-19 15:44:58 +0800

    [MarsRover] feat: add executeCommand, constructor, direction data member; update showStatus

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -2,9 +2,19 @@ package org.example;
 
 public class MarsRover {
 
-    public String showStatus() {
-        return "0:0:N";
+    private String direction;
+
+    public MarsRover(){
+        direction = "N";
     }
 
+    public String showStatus() {
+        return "0:0:" + direction;
+    }
 
+    public String executeCommand(String command) {
+        direction = "W";
+
+        return "0:0:" + direction;
+    }
 }
