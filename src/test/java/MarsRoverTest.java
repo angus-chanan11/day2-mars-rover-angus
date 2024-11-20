dd14413b4f5fc6b3c5b376e643bd124ff9def1e8
angus-chanan11 <angus.chan@oocl.com>
2024-11-20 17:59:12 +0800

    [MarsRover] refactor: remove unused import in test

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -2,7 +2,6 @@ import org.example.MarsRover;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MarsRoverTest {
     @Test
